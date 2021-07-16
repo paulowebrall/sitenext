fb094e6a7f8892538eef936dfc9ea4b43ba4b7ed
paulowebrall <paulo.webrall@gmail.com>
2021-07-16 08:23:21 -0300

    Testando 3

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,15 @@ function Home () {
     return <div>
             <h2>Home 2</h2>
             <Botao/>
+            <Botao2/>
             </div>
 }
 
 function Botao () {
     return <button>Clique aqui</button>
+}
+
+function Botao2 () {
+    return <button>Clique aqui 2</button>
 }
 export default Home;
